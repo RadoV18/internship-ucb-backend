8b15c3b6a2bb13052a52588a127b2bf5d18b75b0
Mauricio Flores Pauletti <mauricio.flores.p@ucb.edu.bo>
2023-04-22 13:59:00 -0400

    Removing unwanted code

--- src/main/java/ucb/internship/backend/controllers/AuthenticationController.java
+++ src/main/java/ucb/internship/backend/controllers/AuthenticationController.java
@@ -6,14 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ucb.internship.backend.dtos.PostLoginDTO;
-import ucb.internship.backend.mailing.EmailVariables;
-import ucb.internship.backend.mailing.Recipient;
-import ucb.internship.backend.mailing.Template;
 import ucb.internship.backend.models.User;
-import ucb.internship.backend.services.EmailService;
 import ucb.internship.backend.services.UserService;
-
-import java.util.Hashtable;
 
 @RestController
 @RequestMapping("/login")
@@ -22,8 +16,6 @@ public class AuthenticationController
     @Autowired
     UserService userService;
 
-    @Autowired
-    EmailService emailService;
     /**
      * @param body Request body with the user email and password
      * @return the user if the email and password are correct, will serve UNAUTHORIZED otherwise
@@ -31,11 +23,6 @@ public class AuthenticationController
     @PostMapping
     public ResponseEntity<User> authenticate(@Valid @RequestBody PostLoginDTO body)
     {
-        var aux = new Hashtable<String, String>();
-        aux.put(EmailVariables.VERIFICATION_CODE.get(), "469831");
-
-        emailService.sendEmail(new Recipient("maofloresp@gmail.com", "Maofloresp"), aux , Template.VERIFICATION_CODE);
-
         var user = userService.authenticate(body.email(), body.password());
 
         if ( user == null)
