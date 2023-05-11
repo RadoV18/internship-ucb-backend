1f2cd1bce2f15674fbc7552dd8fcc17ecb1fff05
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-11 12:08:33 -0400

    UCB-10 add: endpoint for getting all the cities

--- src/main/java/ucb/internship/backend/repositories/CityRepository.java
+++ src/main/java/ucb/internship/backend/repositories/CityRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ucb.internship.backend.models.City;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+
+
     City findByCityId(Integer cityId);
 }
