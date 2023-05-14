0cd75ecf324dd96df6792e012040ab22cdf82901
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-14 18:42:32 -0400

    UCB-9 fix indentation

--- src/main/java/ucb/internship/backend/services/impl/InternshipServiceImpl.java
+++ src/main/java/ucb/internship/backend/services/impl/InternshipServiceImpl.java
@@ -126,6 +126,6 @@ public class InternshipServiceImpl implements InternshipService {
     }
 
     public List<Internship> getInternshipById(Integer id){
-     return  internshipRepository.findByInternshipId(id);
+        return  internshipRepository.findByInternshipId(id);
     }
 }
