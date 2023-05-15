246e115cbf703e50ab3ad10df4188aebb4d164d0
ArielMonroy <ariel.monroy@ucb.edu.bo>
2023-05-15 18:49:13 -0400

    UCB-79 adding endpoting for update phone

--- src/main/java/ucb/internship/backend/controllers/PersonController.java
+++ src/main/java/ucb/internship/backend/controllers/PersonController.java
@@ -1,5 +1,6 @@
 package ucb.internship.backend.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ucb.internship.backend.dtos.PersonDto;
 import ucb.internship.backend.dtos.ResponseDto;
+import ucb.internship.backend.models.Person;
 import ucb.internship.backend.services.PersonService;
 
 @RestController
@@ -18,10 +20,24 @@ public class PersonController {
     }
 
     @PutMapping("/person")
-    public ResponseEntity<ResponseDto<String>> updatePerson(@RequestBody PersonDto personDto, @RequestParam MultipartFile cv) throws Exception {
+    public ResponseEntity<ResponseDto<String>> updatePerson(@RequestParam(value = "data")String data,
+                                                            @RequestParam(required = false, value = "cvFile") MultipartFile cv ) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
         try{
+            PersonDto personDto = objectMapper.readValue(data, PersonDto.class);
             String response = personService.updatePerson(personDto, cv);
             return new ResponseEntity<>(new ResponseDto<String>(response,"",true),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/person")
+    public ResponseEntity<ResponseDto<Person>> getPersonByEmail(@RequestParam String email) {
+        try{
+            Person person = personService.getPersonByEmail(email);
+            return new ResponseEntity<>(new ResponseDto<Person>(person,"",true),HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- src/main/java/ucb/internship/backend/services/impl/PersonServiceImpl.java
+++ src/main/java/ucb/internship/backend/services/impl/PersonServiceImpl.java
@@ -1,5 +1,7 @@
 package ucb.internship.backend.services.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,7 @@ import ucb.internship.backend.services.PersonService;
 public class PersonServiceImpl implements PersonService {
     private PersonRepository personRepository;
     private FileStorageService fileStorageService;
+    private static final Logger logger = LoggerFactory.getLogger(PersonServiceImpl.class);
     @Autowired
     public PersonServiceImpl(PersonRepository personRepository, FileStorageService fileStorageService) {
         this.personRepository = personRepository;
@@ -22,11 +25,21 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     public String updatePerson(PersonDto personDto, MultipartFile cv) throws FileStorageException {
+        logger.info("personDto {}",personDto);
         Person person = personRepository.findById(personDto.getPersonId()).orElseThrow();
-        S3Object s3Object = fileStorageService.createObject(cv);
-        person.setS3Cv(s3Object.getS3ObjectId());
+        if(cv != null){
+            logger.info("cv name {}, contentType {}",cv.getOriginalFilename(),cv.getContentType());
+        }
+        //S3Object s3Object = fileStorageService.createObject(cv);
+        //person.setS3Cv(s3Object.getS3ObjectId());
         person.setPhoneNumber(personDto.getPhoneNumber());
         personRepository.save(person);
         return "Updated";
+    }
+
+    @Override
+    public Person getPersonByEmail(String email) {
+
+        return personRepository.findPersonByEmail(email);
     }
 }
