3954c5f2daff6a3995084c853b25daceda237010
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-29 17:42:07 -0400

    UCB-79 uncommented s3 upload method

--- src/main/java/ucb/internship/backend/dtos/ProfileDto.java
+++ src/main/java/ucb/internship/backend/dtos/ProfileDto.java
@@ -1,7 +1,14 @@
 package ucb.internship.backend.dtos;
 
+import lombok.*;
+
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class ProfileDto {
     Long personId;
     String firstName;
@@ -11,90 +18,6 @@ public class ProfileDto {
     Integer semester;
     Date graduationDate;
     String major;
-    String url;
+    String profilePicture;
     boolean isGraduate;
-
-    public ProfileDto() {
-    }
-
-    public Long getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(Long personId) {
-        this.personId = personId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCi() {
-        return ci;
-    }
-
-    public void setCi(String ci) {
-        this.ci = ci;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Integer getSemester() {
-        return semester;
-    }
-
-    public void setSemester(Integer semester) {
-        this.semester = semester;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getMajor() {
-        return major;
-    }
-
-    public void setMajor(String major) {
-        this.major = major;
-    }
-
-    public Date getGraduationDate() {
-        return graduationDate;
-    }
-
-    public void setGraduationDate(Date graduationDate) {
-        this.graduationDate = graduationDate;
-    }
-
-    public boolean isGraduate() {
-        return isGraduate;
-    }
-
-    public void setGraduate(boolean graduate) {
-        isGraduate = graduate;
-    }
-
 }
